cf8226ff4d79d8cb80742a6725698d9c961e8e1a
allissonelizabethcarmonamercad-lang <allissonelizabethcarmonamercad@gmail.com>
2026-06-09 22:18:14 -0600

    Update MauiProgram.cs with dependency injection

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,18 +1,29 @@
+using BloomBeauty.Services;
+using CommunityToolkit.Mvvm;
+using Microsoft.Maui.Controls.Hosting;
+
 namespace BloomBeauty;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+            .Services.AddSingleton<ProductService>()
+            .AddSingleton<CartService>()
+            .AddSingleton<MainPage>()
+            .AddSingleton<CartPage>()
+            .AddSingleton<FavoritesPage>()
+            .AddSingleton<ProfilePage>();
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
